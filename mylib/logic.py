99e65389664da34f1dc37fb22d6af29dbd8854de
Thomas Lockney <thomas@lockney.net>
2022-08-20 13:26:34 -0700

    Formatted and updated

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,6 +1,7 @@
 import wikipedia
 
-def wiki(name='War Goddess', length=1):
-    """"""
+
+def wiki(name="War Goddess", length=1):
+    """Grab wikipedia summary"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
